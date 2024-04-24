969f6837173abd52f724b2f782bf982aa1c2c6fe
varun chaaras <chaaravasvarun@gmail.com>
2024-04-24 15:52:38 +0530

    Initial Commit

---
+++ Virtual Library/Virtual_LibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Virtual_LibraryApp.swift
+//  Virtual Library
+//
+//  Created by Varun Chaaras on 24/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Virtual_LibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
